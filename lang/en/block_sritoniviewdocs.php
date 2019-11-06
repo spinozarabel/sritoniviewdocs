a1cd0bf2ed5569ec3ff88f0cfb1e500d955c75f8
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2019-11-05 21:31:52 -0800

    initial commit

---
+++ lang/en/block_sritoniviewdocs.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Strings for component 'block_newblock', language 'en'
+ *
+ * @package   block_sritoniviewdocs
+ * @copyright Madhu Avasarala <info@headstart.edu.in>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['blockstring'] = 'Block string';
+$string['descconfig'] = 'Description of the config section';
+$string['descfoo'] = 'Config description';
+$string['headerconfig'] = 'Config section header';
+$string['labelfoo'] = 'Config label';
+$string['sritoniviewdocs:addinstance'] = 'Add a sritoniviewdocs block';
+$string['sritoniviewdocs:myaddinstance'] = 'Add a sritoniviewdocs block to my moodle';
+$string['pluginname'] = 'Sritoni View Your Docs';
+$string['sritoniviewdocs'] = 'SriToni View Your Docs';
